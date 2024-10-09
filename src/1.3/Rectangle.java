2197a6ca30dffac3582e0bc04916df4b1a548d8b
danh23ns <154571296+danh23ns@users.noreply.github.com>
2024-10-09 23:12:29 +0700

    Add files via upload

---
+++ src/1.3/Rectangle.java
@@ -0,0 +1,40 @@
+public class Rectangle {
+    private float length;
+    private float width;
+
+    public Rectangle(){
+        length = 1.0f;
+        width = 1.0f;
+
+    }
+    public Rectangle(float l, float w){
+        length = l;
+        width = w;
+
+    }
+    public float getLength(){
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+    public double getArea(){
+        return length*width;
+    }
+    public double getPerimeter(){
+        return (length+width)*2;
+    }
+    public String toString(){
+        return "Rectangle[ length="+length+", width="+width+"]";
+    }
+
+}
