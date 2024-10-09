ce97fb02f29e04c2415f640b7bc95841799f2e69
danh23ns <154571296+danh23ns@users.noreply.github.com>
2024-10-09 23:11:03 +0700

    Add files via upload

---
+++ src/1.2/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(1.1);
+        System.out.println(c1);
+        Circle c2 = new Circle();
+        System.out.println(c2);
+
+        c1.setRadius(2.2);
+        System.out.println(c1);
+        System.out.println("radius is: " + c1.getRadius());
+
+        System.out.printf("area is: %.2f%n", c1.getArea());
+        System.out.printf("circumference is: %.2f%n", c1.getCircumference());
+
+    }
+
+}
