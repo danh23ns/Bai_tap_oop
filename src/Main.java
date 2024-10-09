dc4c0cca4d0a007f81754899bddfa6e1eb9240de
danh23ns <danhnht.23ns@vku.udn.vn>
2024-10-09 22:28:13 +0700

    init project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(1.0);
+        System.out.println("The circle has radius of " +c1.getRadius() + " and area of " + c1.getArea());
+
+        Circle c2 = new Circle(2.0);
+        System.out.println("The circle has radius of " +c2.getRadius() + " and area of " + c2.getArea());
+
+
+    }
+}
